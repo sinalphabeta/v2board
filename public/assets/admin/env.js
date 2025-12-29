42f70493f959c62a88d5f303277fcfab81db66bb
sinalphabeta <53267553+sinalphabeta@users.noreply.github.com>
2025-12-29 22:17:39 +0800

    feat: CF Pages Variables

---
+++ public/assets/admin/env.js
@@ -0,0 +1,17 @@
+window.settings = {
+  // 站点标题
+  title: 'REPLACE_TITLE',
+  // API
+  host: 'REPLACE_API_HOST',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'REPLACE_BACKGROUND_URL',
+  logo: 'REPLACE_LOGO',
+  // 需与V2Board设置中的后台路径一致
+  secure_path: 'REPLACE_SECURE_PATH'
+}
